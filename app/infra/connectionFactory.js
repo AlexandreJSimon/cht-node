db08a600d7b9b93c5b0891d4c5964e2869755119
Alexandre <alexandre.j.simon@gmail.com>
2019-03-09 17:49:20 -0300

    update packeges

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 function createDBConnection() {
     if(!process.env.NODE_ENV){
         return mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : '',
-            database : 'cht_db'
+            host : 'us-cdbr-iron-east-03.cleardb.net',
+            user : 'bdfb70cb56f0f5',
+            password : '4c9699d8',
+            database : 'heroku_8186b6c545153e6'
         });
     }
     if(process.env.NODE_ENV == 'test'){
